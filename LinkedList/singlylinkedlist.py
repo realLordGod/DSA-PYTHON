57db250876aa65c83d11c6fc8b16ea69df7aa7b6
niryn <nautiyalaman93@gmail.com>
2026-06-22 17:34:25 +0530

    update

--- LinkedList/singlylinkedlist.py
+++ LinkedList/singlylinkedlist.py
@@ -1,30 +1,88 @@
 class Node:
-    def __init__(self,info,next):
-        self.info=info
+
+    def __init__(self,val,next=None):
+        self.val=val
         self.next=next
-        
 
 
-
-
+    
 class SinglyLinkedList:
+
     def __init__(self,head=None):
         self.head = head
-    
 
-    def insertAtEnd(self,value):
-        temp = Node(value)
 
-        if (self.head!=None):
-            t1=self.head
+    def insertAtBeg(self,val):
+        self.head = Node(val,self.head)
 
-            while(t1.next!=None):
-                t1= t1.next
+    def insertInMid(self,val,x):
+        temp = Node(val)
+        t1 = self.head
+        while t1:
+            if t1.val == x:
+                temp.next=t1.next
+                t1.next=temp
+                break
 
-            t1.next=temp
+            t1 = t1.next
 
+
+    def inserAtEnd(self,val):
+        temp=Node(val)
+
+        t1=self.head
+
+        if self.head!=None:
+            while t1:
+                if t1.next==None:
+                    t1.next=temp
+                    return
+                t1=t1.next
 
         else:
-            self.head = temp
+            self.head=temp
+
+    def deleteLL(self,val):
+
+        if self.head ==None:
+            return
+        
+        if self.head.val == val:
+            self.head = self.head.next
+            return
+
+
+
+
+
+        t1=self.head
+        prev=t1
+
+        while t1:
+            if t1.val == val:
+                prev.next = t1.next
+                return
+                
+            else:
+                prev=t1
+                t1=t1.next
+
+
+    def Printall(self):
+
+        t1=self.head
+
+        while t1:
+            print(t1.val)
+            t1=t1.next
+            
+
+
+
+    
+    
+
+            
+        
 
         
